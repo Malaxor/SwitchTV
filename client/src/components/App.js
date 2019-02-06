fd36d403fe6b3b01b40d02737ebca28cf7247c0a
Malaxor <nogobaduk@gmail.com>
2019-02-05 18:36:01 -0700

    routes working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const App = () => {
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-    return <div>Hello App</div>;
-};
+const App = () => (
+
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
+        </Switch>
+    </BrowserRouter>
+);
 export default App;
