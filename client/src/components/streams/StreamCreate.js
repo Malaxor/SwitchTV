60c4418e6d53f9ec6dd50985e571d181a24e90bc
Malaxor <nogobaduk@gmail.com>
2019-02-12 09:29:30 -0700

    created validate function

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -32,6 +32,21 @@ class StreamCreate extends Component {
     }
 }
 
+const validate = formValues => {
+
+    const errors = {};
+
+    if(!formValues.title) {
+
+        errors.title = 'You must enter a title';
+    }
+    if(!formValues.description) {
+
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+};
+
 export default reduxForm({
 
     form: 'streamCreate'
