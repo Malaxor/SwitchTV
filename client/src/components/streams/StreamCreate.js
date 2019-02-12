33d7517e8a51c3fe980f70222643a817b810ee4c
Malaxor <nogobaduk@gmail.com>
2019-02-12 09:22:21 -0700

    wired form submit

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -4,20 +4,29 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
 
-    renderInput({ input }) {
+    renderInput({ input, label }) {
 
         return (
-
-            <input {...input} />
-        ); 
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
     }
+
+    onSubmit(formValues) {
+
+        
+    }
+
     render() {
         
         return (
-
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            // handleSubmit props object provided by redux-form
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className='ui button primary'>Submit</button>
             </form>
         );
     }
