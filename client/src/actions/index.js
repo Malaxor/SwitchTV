af570ef8dada12df99c6245fcf7a5d4c34e80f38
Malaxor <nogobaduk@gmail.com>
2019-02-13 19:30:04 -0700

    axios setup, action creater for setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 
+import streams from '../api/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
 
 export const setSignIn = userId => ({
@@ -8,3 +9,8 @@ export const setSignIn = userId => ({
 });
 
 export const setSignOut = () => ({ type: SIGN_OUT });
+
+export const createStream = formValues => async dispatch => {
+
+    streams.post('/streams/', formValues);
+};
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,6 +1,9 @@
 
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+
 
 class StreamCreate extends Component {
 
