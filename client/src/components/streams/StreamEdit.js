205072d1731f0c204e7e072b6e8d8293ef89b22c
Malaxor <nogobaduk@gmail.com>
2019-02-21 17:29:39 -0700

    working on editStream component (url-based) selection for editing stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,8 +1,14 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
 
+    console.log(props)
     return <div>StreamEdit</div>;
 }
-export default StreamEdit;
+
+// ownProps object is the same as props object available in the StreamEdit component
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(mapStateToProps)(StreamEdit);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -16,8 +16,8 @@ class StreamList extends Component {
 
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">Edit</button>
-                    <button className="ui button negative">Delete</button>
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
                 </div>
             );    
         }
