c220a33ba0c4d1c5e3e0fa95113dd2e798d6b537
Malaxor <nogobaduk@gmail.com>
2019-02-25 19:27:00 -0700

    making the modal reusable with props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,14 +1,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = props => {
 
     return ReactDOM.createPortal(
 
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                What a bunch of crap
+        <div onClick={() => history.push('/streams/delete')} className="ui dimmer modals visible active">
+            <div onClick={e => e.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>, document.querySelector('#modal')
     );
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -4,10 +4,22 @@ import Modal from '../Modal';
 
 const StreamDelete = () => {
 
+    const actions = (
+        // React Fragment fixes styling issue: there used to be a div element in its place.
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    ); 
+
     return (
         <div>
             Stream Delete
-            <Modal />
+            <Modal 
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     )
 }
