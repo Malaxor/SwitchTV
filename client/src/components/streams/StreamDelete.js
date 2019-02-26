74e8306a8ff1b471f6c0ba4f3b268a2bb31ed3b5
Malaxor <nogobaduk@gmail.com>
2019-02-25 19:48:02 -0700

    working on delete stream view

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,26 +1,39 @@
 
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-    const actions = (
-        // React Fragment fixes styling issue: there used to be a div element in its place.
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    ); 
+    componentDidMount() {
 
-    return (
-        <div>
-            Stream Delete
-            <Modal 
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-            />
-        </div>
-    )
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    renderActions() {
+
+        return (
+            // React Fragment fixes styling issues: there used to be a div element in its place.
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>
+        );    
+    }
+    render() {
+
+        return (
+            <div>
+                Stream Delete
+                <Modal 
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div>
+        )
+    }
 }
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
