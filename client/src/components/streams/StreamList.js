12494cd6ebebd5ac58b038b90c8a9bda09c3e60f
Malaxor <nogobaduk@gmail.com>
2019-02-26 17:37:54 -0700

    setup video element

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -27,7 +27,7 @@ class StreamList extends Component {
         return this.props.streams.map(stream => {
 
             return (
-                <div className="item" key={stream.id}>3
+                <div className="item" key={stream.id}>
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,17 @@
 
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
+
+    }
     componentDidMount() {
 
         this.props.fetchStream(this.props.match.params.id);
@@ -19,6 +26,11 @@ class StreamShow extends Component {
 
         return (
             <div>
+                <video 
+                    ref={this.videoRef}
+                    style={{ width: '100%' }}
+                    controls
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
