edbb39ed1078cbe3353b48044e4545bf8470fd40
Malaxor <nogobaduk@gmail.com>
2019-02-26 17:06:32 -0700

    rendering a particular stream

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,8 +1,30 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
+class StreamShow extends Component {
 
-    return <div>StreamShow</div>;
+    componentDidMount() {
+
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+
+        if(!this.props.stream) {
+            
+            return <div>Loading stream...</div>;
+        }
+        const { title, description } = this.props.stream;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
 }
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
